2055c249f23b8e166b67a3bab11d2a7fac6fadf3
Roman Novikov <r.novikov.zp.ua@gmail.com>
2023-06-23 14:38:45 +0300

    Add grid
    Add function, making grid in the sketch field

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,17 @@
-
+let fieldSize = 16; // Value got from the size slider
+const sketchField = document.getElementById("sketch-field"); // Value stores the sketch-field div
+let cellColor = '#bb00ff'; //Value got from the color picker
+let squareQuantity = 256; // Quantity of squares to draw
 /*Size-slider script*/
 
 const sizeSlider = document.getElementById("size-slider");
 let sizeSliderValue = document.getElementById("size-slider-value");
 
 sizeSlider.addEventListener("input", function() {
-  sizeSliderValue.textContent = this.value;
+  fieldSize = this.value;
+  squareQuantity = fieldSize * fieldSize;
+  sizeSliderValue.textContent = `${fieldSize} x ${fieldSize}`;
+  console.log(`Field size is ${fieldSize}.`);
 });
 
 /*Color-picker script*/
@@ -14,5 +20,23 @@ const colorPicker = document.getElementById("color-picker");
 let colorPickerValue = document.getElementById("color-picker-value");
 
 colorPicker.addEventListener("input", function() {
-  colorPickerValue.textContent = this.value;
+  cellColor = this.value;
+  colorPickerValue.textContent = cellColor;
+  console.log(`Picked color ${cellColor}.`);
 });
+
+/*Draw squares script*/
+
+function drawSquares(squareQuantity) {
+  const height = sketchField.offsetHeight;
+  for (let i = 0; i < squareQuantity; i++) {
+    let cell = document.createElement('div');
+    cell.style.height = `${height / fieldSize - 5}px`;
+    cell.style.width = `${height / fieldSize - 5}px`;
+    cell.style.backgroundColor = '#c4bbf0';
+    sketchField.appendChild(cell);
+  }
+};
+
+drawSquares(squareQuantity);
+
