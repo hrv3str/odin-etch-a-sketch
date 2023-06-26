ff86de876ee6ff6db137db6c2fd65bd2119510ff
Roman Novikov <r.novikov.zp.ua@gmail.com>
2023-06-26 11:03:11 +0300

    'clearField' function
    Add 'clearField' function to reset the sketch field, join
    it with the slider to regenerate field with new size values
    
    Fix the flex-box paremeters to get the proper grid
    
    Test on every slider value

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,30 @@ let fieldSize = 16; // Value got from the size slider
 const sketchField = document.getElementById("sketch-field"); // Value stores the sketch-field div
 let cellColor = '#bb00ff'; //Value got from the color picker
 let squareQuantity = 256; // Quantity of squares to draw
+
+/*Draw squares script*/
+
+function drawSquares(squareQuantity) {
+  const height = sketchField.offsetHeight;
+  for (let i = 0; i < squareQuantity; i++) {
+    let cell = document.createElement('div');
+    cell.style.height = `${height / fieldSize - 0.5}px`;
+    cell.style.width = `${height / fieldSize - 0.5}px`;
+    cell.style.backgroundColor = '#c4bbf0';
+    sketchField.appendChild(cell);
+  }
+};
+
+drawSquares(squareQuantity);
+
+/* Clear field script*/
+function clearField() {
+while (sketchField.firstChild) {
+  sketchField.removeChild(sketchField.firstChild);
+}
+};
+
+
 /*Size-slider script*/
 
 const sizeSlider = document.getElementById("size-slider");
@@ -11,6 +35,8 @@ sizeSlider.addEventListener("input", function() {
   fieldSize = this.value;
   squareQuantity = fieldSize * fieldSize;
   sizeSliderValue.textContent = `${fieldSize} x ${fieldSize}`;
+  clearField();
+  drawSquares(squareQuantity);
   console.log(`Field size is ${fieldSize}.`);
 });
 
@@ -25,18 +51,4 @@ colorPicker.addEventListener("input", function() {
   console.log(`Picked color ${cellColor}.`);
 });
 
-/*Draw squares script*/
-
-function drawSquares(squareQuantity) {
-  const height = sketchField.offsetHeight;
-  for (let i = 0; i < squareQuantity; i++) {
-    let cell = document.createElement('div');
-    cell.style.height = `${height / fieldSize - 5}px`;
-    cell.style.width = `${height / fieldSize - 5}px`;
-    cell.style.backgroundColor = '#c4bbf0';
-    sketchField.appendChild(cell);
-  }
-};
-
-drawSquares(squareQuantity);
 
